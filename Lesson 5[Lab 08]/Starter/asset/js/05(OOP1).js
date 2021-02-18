1dc43cc8221389fc1f844c9d9c4c9747c1c07d64
Tolosa-mitiku <se.tolosa.mitiku@gmail.com>
2021-02-18 11:16:46 +0300

    task 7 complete

--- Lesson 5[Lab 08]/Starter/asset/js/05(OOP1).js
+++ Lesson 5[Lab 08]/Starter/asset/js/05(OOP1).js
@@ -2,9 +2,13 @@
 function Person(firstName, lastName) {
 
     // 1. set the firstName and lastName using this keyword
-
-
+    this.firstName = firstName;
+    this.lastName = lastName;
     //2. Create a greeting function returns [use this keyword to get the first and last name]
+    this.greeting = function() {
+        console.log("Hi, " + this.firstName + this.lastName);
+
+    }
     //  ==> Hi , firstName lastName
 
 
@@ -13,6 +17,6 @@ function Person(firstName, lastName) {
 
 //3. Create Jon Whose full name is ["Jon", "Snow"] Object using new keyword [new ConstructorFun()]
 
-
+    Jon = new Person("jon", "snow")
 //4. Display the greeting on Jon Object [remove the string when you have the object]
-Obj_C1.innerHTML = "Jon.greeting()";
+Obj_C1.innerHTML = Jon.greeting();
