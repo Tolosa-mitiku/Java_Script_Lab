db8cb2f9cb9261258118cecd2cd959df34cd3fb1
Tolosa-mitiku <se.tolosa.mitiku@gmail.com>
2021-02-18 11:28:11 +0300

    task 9 complete

--- Lesson 5[Lab 08]/Starter/asset/js/05(OOP3).js
+++ Lesson 5[Lab 08]/Starter/asset/js/05(OOP3).js
@@ -1,16 +1,21 @@
 // 1. Create a Person_ES6 Class 
+class Person {
+    //2. Create the constructor that initialize firstName and lastName
+    constructor (firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    }
+    //3. Define greeting() method that displays Full Name
 
+    greeting() {
+        return ('Hi' + ' ' + this.firstName + this.lastName);
+    }
 
-
-//2. Create the constructor that initialize firstName and lastName
-
-
-//3. Define greeting() method that displays Full Name
-
+}
 
 //4. Create  Object using ES6 class by passing firstName,lastName ["Bran Stark"]
-
+    Bran = new Person("Bran","Stark")
 
 
 //5. Display the greeting on Bran Object [remove the string when you have the object]
-Obj_C3.innerHTML = "Bran.greeting()";
+Obj_C3.innerHTML = Bran.greeting();
